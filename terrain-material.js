2d8d12c2fdd5f4933abc0e21bfa3e8cdf2142afe
aryasalehi <64514807+aryasalehi@users.noreply.github.com>
2022-09-12 19:20:57 +0430

    add biomes blending to the shader

--- terrain-material.js
+++ terrain-material.js
@@ -46,22 +46,28 @@ const loadTerrainMaterial = async () => {
   );
   rockNormalMap.encoding = THREE.LinearEncoding;
 
-  const grassDiffMap = await _loadTexture('assets/textures/grass/grass.png');
+  const grassDiffMap = await _loadTexture('assets/textures/grass/grass4.png');
   grassDiffMap.encoding = THREE.sRGBEncoding;
 
-  const rockMap = await _loadTexture('assets/textures/grass/rock.png');
+  const grassNormalMap = await _loadTexture('assets/textures/grass/grass4n.png');
+  grassNormalMap.encoding = THREE.LinearEncoding;
+
+  const rockMap = await _loadTexture('assets/textures/grass/rock6.png');
   rockMap.encoding = THREE.sRGBEncoding;
+
+  const rockNormal = await _loadTexture('assets/textures/grass/rock6n.png');
+  rockMap.encoding = THREE.LinearEncoding;
 
   const noiseTexture = await _loadTexture('assets/textures/simplex-noise.png');
   noiseTexture.encoding = THREE.LinearEncoding;
 
   // define material uniforms here
   const materialUniforms = {
-    uDiffMap: { value: rockMap },
-    uRoughnessMap: { value: rockRoughnessMap },
-    uNormalMap: { value: rockNormalMap },
-    uAoMap: { value: rockAoMap },
-    uGrassDiff: { value: grassDiffMap},
+    uDiffMap: { value: [grassDiffMap, rockMap] },
+    uRoughnessMap: { value: [rockRoughnessMap] },
+    uNormalMap: { value: [grassNormalMap, rockNormal] },
+    uAoMap: { value: [rockAoMap] },
+    uGrassDiff: { value: grassDiffMap },
     uNoiseTexture: { value: noiseTexture },
   };
 
@@ -79,13 +85,26 @@ const loadTerrainMaterial = async () => {
       // vertex shader
       const uvParseVertex = glsl`
         #include <uv_pars_vertex>
+
+        // attribute ivec4 biomeTypes = ivec4(0); // TODO: implement biome types as an attribute
+        // attribute vec4 biomeWeights = vec4(1., 0., 0., 0.);
+
+        flat varying ivec4 vBiomeTypes; 
+        varying vec4 vBiomeWeights;
+
         varying mat3 vNormalMatrix;
         varying vec3 vPosition;
         varying vec3 vObjectNormal;
+
+        ivec4 biomeTypes = ivec4(0);
+        vec4 biomeWeights = vec4(1., 1., 1., 1.);
       `;
 
       const worldPosVertex = glsl`
        #include <worldpos_vertex>
+
+       vBiomeTypes = biomeTypes;
+       vBiomeWeights = biomeWeights;
 
        vPosition = transformed;
        vNormalMatrix = normalMatrix;
@@ -95,31 +114,40 @@ const loadTerrainMaterial = async () => {
       // fragment shader
       const mapParseFragment = glsl`
         #include <map_pars_fragment>
-  
+
+        // precision highp sampler2DArray;
+
+        flat varying ivec4 vBiomeTypes; 
+        varying vec4 vBiomeWeights;
+
         varying vec3 vPosition;
         varying mat3 vNormalMatrix;
         varying vec3 vObjectNormal;
   
-        uniform sampler2D uDiffMap;
-        uniform sampler2D uRoughnessMap;
-        uniform sampler2D uNormalMap;
-        uniform sampler2D uAoMap;
+        uniform sampler2D uDiffMap[2];
+        uniform sampler2D uRoughnessMap[2];
+        uniform sampler2D uNormalMap[2];
+        uniform sampler2D uAoMap[2];
 
+        uniform sampler2D uNoiseTexture;
         uniform sampler2D uGrassDiff;
   
-        uniform sampler2D uNoiseTexture;
-  
-        float TRI_SCALE = 0.05;
+        float TRI_SCALE = 0.1;
         float TRI_SHARPNESS = 0.5;
-  
+
+        vec4 blendSamples(vec4 samples[4], vec4 weights) {
+          float weightSum = weights.x + weights.y + weights.z + weights.w;
+          return (samples[0] * weights.x + samples[1] * weights.y + samples[2] * weights.z + samples[3] * weights.w) / weightSum;
+        }
+
         float sum( vec3 v ) { return v.x+v.y+v.z; }
-  
+
         vec4 hash4(vec2 p) {
           return fract(sin(vec4(1.0 + dot(p, vec2(37.0, 17.0)), 2.0 + dot(p, vec2(11.0, 47.0)), 3.0 + dot(p, vec2(41.0, 29.0)), 4.0 + dot(p, vec2(23.0, 31.0)))) * 103.0);
         }
-  
+
         // ! based on this article : https://iquilezles.org/articles/texturerepetition
-        vec4 textureNoTile( sampler2D samp, in vec2 uv  ) {
+        vec4 textureNoTile(sampler2D textureSample, int textureIndex, in vec2 uv ) {
           float k = vec3(texture2D(uNoiseTexture, 0.0025*uv)).x; // cheap (cache friendly) lookup
           float l = k*8.0;
           float f = fract(l);
@@ -127,65 +155,87 @@ const loadTerrainMaterial = async () => {
           float ia = floor(l+0.5); // suslik's method (see comments)
           float ib = floor(l);
           f = min(f, 1.0-f)*2.0;
-  
-          vec2 offa = vec2(hash4(vec2(300.0,7.0)*ia)); // can replace with any other hash
-          vec2 offb = vec2(hash4(vec2(300.0,7.0)*ib)); // can replace with any other hash
-  
-          vec4 cola = texture2D(samp, vec2(uv + offa));
-          vec4 colb = texture2D(samp, vec2(uv + offb));
-  
+
+          vec2 offa = vec2(hash4(vec2(30.0,7.0)*ia)); // can replace with any other hash
+          vec2 offb = vec2(hash4(vec2(30.0,7.0)*ib)); // can replace with any other hash
+
+          vec4 cola = texture2D(textureSample, vec2(uv + offa));
+          vec4 colb = texture2D(textureSample, vec2(uv + offb));
+
           return mix(cola, colb, smoothstep(0.2,0.8,f-0.1*sum(cola.xyz-colb.xyz)));
         }
-  
-        vec4 triplanarMap(vec3 inputPosition, vec3 inputNormal, sampler2D inputTexture){
+
+        vec4 blendBiomes(sampler2D inputTextures[2], vec2 uv) {
+          vec4 samples[4];
+
+          // samples[0] = textureNoTile(inputTextures, vBiomeTypes.x, uv);
+          // samples[1] = textureNoTile(inputTextures, vBiomeTypes.y, uv);
+          // samples[2] = textureNoTile(inputTextures, vBiomeTypes.z, uv);
+          // samples[3] = textureNoTile(inputTextures, vBiomeTypes.w, uv);
+          float slope = max(0.f, 1.f - vObjectNormal.y);
+          float fade = clamp(slope + 0.4, 0., 1.);
+          float grass = 1.0 - fade;
+          float rock = fade;
+
+          samples[0] = textureNoTile(inputTextures[0], vBiomeTypes.x, uv);
+          samples[1] = textureNoTile(inputTextures[1], vBiomeTypes.y, uv);
+          samples[2] = vec4(0.);
+          samples[3] = vec4(0.);
+
+          vec4 weights = vec4(grass, rock, 0., 0.);
+
+          return blendSamples(samples, weights);
+        }
+
+        vec4 triplanarMap(vec3 inputPosition, vec3 inputNormal, sampler2D inputTextures[2]){
           vec2 uvX = inputPosition.zy * TRI_SCALE;
           vec2 uvY = inputPosition.xz * TRI_SCALE;
           vec2 uvZ = inputPosition.xy * TRI_SCALE;
           
-          vec4 colX = textureNoTile(inputTexture, uvX);
-          vec4 colY = textureNoTile(inputTexture, uvY);
-          vec4 colZ = textureNoTile(inputTexture, uvZ);
-  
+          vec4 colX = blendBiomes(inputTextures, uvX);
+          vec4 colY = blendBiomes(inputTextures, uvY);
+          vec4 colZ = blendBiomes(inputTextures, uvZ);
+ 
           vec3 blendWeight = pow(abs(inputNormal), vec3(TRI_SHARPNESS));
           blendWeight /= dot(blendWeight,vec3(1));
-  
+
           return colX * blendWeight.x + colY * blendWeight.y + colZ * blendWeight.z;
         }
-  
-        vec4 triplanarNormal(vec3 inputPosition, vec3 inputNormal, sampler2D inputTexture) {
+
+        vec4 triplanarNormal(vec3 inputPosition, vec3 inputNormal, sampler2D inputTextures[2]) {
           // Tangent Reconstruction
           // Triplanar uvs
           vec2 uvX = inputPosition.zy * TRI_SCALE;
           vec2 uvY = inputPosition.xz * TRI_SCALE;
           vec2 uvZ = inputPosition.xy * TRI_SCALE;
           
-          vec4 colX = textureNoTile(inputTexture, uvX);
-          vec4 colY = textureNoTile(inputTexture, uvY);
-          vec4 colZ = textureNoTile(inputTexture, uvZ);
-  
+          vec4 colX = blendBiomes(inputTextures, uvX);
+          vec4 colY = blendBiomes(inputTextures, uvY);
+          vec4 colZ = blendBiomes(inputTextures, uvZ);
+
           // Tangent space normal maps
           vec3 tx = colX.xyz * vec3(2,2,2) - vec3(1,1,1);
           vec3 ty = colY.xyz * vec3(2,2,2) - vec3(1,1,1);
           vec3 tz = colZ.xyz * vec3(2,2,2) - vec3(1,1,1);
           vec3 weights = abs(inputNormal);
           weights = weights / (weights.x + weights.y + weights.z);
-  
+
           // Get the sign (-1 or 1) of the surface normal
           vec3 axis = sign(inputNormal);
-  
+
           // Construct tangent to world matrices for each axis
           vec3 tangentX = normalize(cross(inputNormal, vec3(0.0, axis.x, 0.0)));
           vec3 bitangentX = normalize(cross(tangentX, inputNormal)) * axis.x;
           mat3 tbnX = mat3(tangentX, bitangentX, inputNormal);
-  
+
           vec3 tangentY = normalize(cross(inputNormal, vec3(0.0, 0.0, axis.y)));
           vec3 bitangentY = normalize(cross(tangentY, inputNormal)) * axis.y;
           mat3 tbnY = mat3(tangentY, bitangentY, inputNormal);
-  
+
           vec3 tangentZ = normalize(cross(inputNormal, vec3(0.0, -axis.z, 0.0)));
           vec3 bitangentZ = normalize(-cross(tangentZ, inputNormal)) * axis.z;
           mat3 tbnZ = mat3(tangentZ, bitangentZ, inputNormal);
-  
+
           // Apply tangent to world matrix and triblend
           // Using clamp() because the cross products may be NANs
           vec3 worldNormal = normalize(
@@ -200,18 +250,13 @@ const loadTerrainMaterial = async () => {
       const mapFragment = glsl`
         #include <map_fragment>
 
-        float slope = max(0.f, 1.f - vObjectNormal.y);
  
         vec4 triplanarDiffColor = triplanarMap(vPosition, vObjectNormal, uDiffMap);
 
-        // float grass = slope / 15.0;
-        // float rock = 1.0 - slope / 15.0;
-        float fade = clamp(slope + 0.5, 0., 1.);
-        float grass = 1.0 - fade;
-        float rock = fade;
-
-        vec4 grassColor = triplanarMap(vPosition, vObjectNormal, uGrassDiff);
-        diffuseColor *= (rock * triplanarDiffColor + grass * grassColor);
+        // vec4 grassColor = triplanarMap(vPosition, vObjectNormal, uDiffMap);
+        // vec4 grassColor = vec4(0.1, 0.8, 0.3, 1.);
+        // diffuseColor *= (rock * triplanarDiffColor + grass * grassColor);
+        diffuseColor *= triplanarDiffColor;
       `;
       const roughnessMapFragment = glsl`
         #include <roughnessmap_fragment>
@@ -223,23 +268,21 @@ const loadTerrainMaterial = async () => {
         #include <normal_fragment_maps>
 
         vec3 triplanarNormalColor = triplanarNormal(vPosition, vObjectNormal, uNormalMap).xyz;
-        if(slope >= 0.15){
-          normal = normalize(vNormalMatrix * triplanarNormalColor);
-        }
+        normal = normalize(vNormalMatrix * triplanarNormalColor);
       `;
 
       const aoMapFragment = glsl`
-        // #include <aomap_fragment>
+        #include <aomap_fragment>
 
-        vec4 triplanarAoColor = triplanarMap(vPosition, vObjectNormal, uAoMap);
+        // vec4 triplanarAoColor = triplanarMap(vPosition, vObjectNormal, uAoMap);
 
-        float ambientOcclusion = triplanarAoColor.r;
-        reflectedLight.indirectDiffuse *= ambientOcclusion;
+        // float ambientOcclusion = triplanarAoColor.r;
+        // reflectedLight.indirectDiffuse *= ambientOcclusion;
 
-        #if defined( USE_ENVMAP ) && defined( STANDARD )
-          float dotNV = saturate( dot( geometry.normal, geometry.viewDir ) );
-          reflectedLight.indirectSpecular *= computeSpecularOcclusion( dotNV, ambientOcclusion, material.roughness );
-        #endif
+        // #if defined( USE_ENVMAP ) && defined( STANDARD )
+        //   float dotNV = saturate( dot( geometry.normal, geometry.viewDir ) );
+        //   reflectedLight.indirectSpecular *= computeSpecularOcclusion( dotNV, ambientOcclusion, material.roughness );
+        // #endif
       `;
 
       // extend shaders
