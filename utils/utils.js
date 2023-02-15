9b55a593ce06da445af8f14096d2be31fa79cede
Avaer Kazmer <a@modules.io>
2023-02-15 14:10:45 -0500

    Update utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,47 +1,64 @@
-import * as THREE from 'three';
-import {NO_LIGHT_MATERIAL_SHADOW_INTENSITY} from '../constants.js';
+export function shuffle(array, rng = Math.random) {
+  let currentIndex = array.length;
+    let randomIndex;
 
-export class ENUM {
-  constructor(namesArray) {
-    for (let i = 0; i < namesArray.length; i++) {
-      const name = namesArray[i];
-      this[name] = i;
-    }
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(rng() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
+
+  return array;
 }
 
-export const _patchOnBeforeCompileFunction = (material, func) => {
-  const previousOnBeforeCompile = material.onBeforeCompile;
-  material.onBeforeCompile = (shader) => {
-    previousOnBeforeCompile(shader);
-    func(shader);
-  };
+//
+
+export const align = (v, N) => {
+  const r = v % N;
+  return r === 0 ? v : v - r + N;
+};
+export const align4 = v => align(v, 4);
+
+export const getClosestPowerOf2 = size => Math.ceil(Math.log2(size));
+export const clamp = (n, min, max) => Math.min(Math.max(n, min), max);
+
+export const getBoundingSize = boundingType => {
+  switch (boundingType) {
+    case 'sphere': return 4;
+    case 'box': return 6;
+    default: return 0;
+  }
 };
 
-export const _addNoLightingShaderChunk = () => {
-  const outgoingLightChunk = /* glsl */ `vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + totalEmissiveRadiance;`;
-  const outgoingLightChunkNoLight = /* glsl */ `vec3 outgoingLight = diffuseColor.rgb * (1.0 - ${NO_LIGHT_MATERIAL_SHADOW_INTENSITY} * (1.0 - getShadowMask()));`;
-
-  const _replaceOutgoingLightChunk = (materialName) => {
-    THREE.ShaderLib[materialName].fragmentShader = THREE.ShaderLib[materialName].fragmentShader.replace(
-      outgoingLightChunk,
-      /* glsl */ `
-        #ifndef NO_LIGHT
-          ${outgoingLightChunk}
-        #else
-          ${outgoingLightChunkNoLight}
-        #endif
-        `
-    );
-  };
-
-  _replaceOutgoingLightChunk("lambert");
-  _replaceOutgoingLightChunk("standard");
+export const lookAtQuaternion = (dirVec)=>{
+  var mx = new THREE.Matrix4().lookAt(new THREE.Vector3(0,0,0), dirVec, new THREE.Vector3(0,1,0));
+  return new THREE.Quaternion().setFromRotationMatrix(mx);
 };
 
+//
 
-export const _disableOutgoingLights = (material) => {
-  material.lights = false;
-  material.defines = material.defines || {};
-  material.defines.NO_LIGHT = true;
-};
+export function makePromise() {
+  let resolve, reject;
+  const p = new Promise((a, r) => {
+    resolve = a;
+    reject = r;
+  });
+  // Object.defineProperty(p, 'accept', {
+  //   get() {
+  //     console.warn('accept get stack', new Error().stack);
+  //   },
+  //   set(v) {
+  //     console.warn('accept set stack', new Error().stack);
+  //   }
+  // });
+  p.resolve = resolve;
+  p.reject = reject;
+  return p;
+}
