62ca505716182859a36414bbefd488f805dc7ea4
Avaer Kazmer <a@modules.io>
2023-02-15 15:52:15 -0500

    Small cleanup

--- procgen/pg-worker-manager.js
+++ procgen/pg-worker-manager.js
@@ -3,7 +3,7 @@ import {abortError} from '../managers/lock-manager.js';
 
 // const workerUrl = '/public/core-modules/heightfield/procgen/pg-worker.js';
 const u = new URL(import.meta.url);
-u.host = globalThis?.location?.host;
+u.host = globalThis.location.host;
 u.pathname += 'procgen/pg-worker.js';
 const workerUrl = u.href;
 
